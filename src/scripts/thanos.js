110c14bad81241078d6de5f29366bf703d83f1be
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-06-08 18:20:03 +0300

    add thanos

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let dbContacts = JSON.parse(data);
+    dbContacts = dbContacts.filter(() => Math.random() < 0.5);
+    await fs.writeFile(PATH_DB, JSON.stringify(dbContacts, null, 4), 'utf-8');
+  } catch (error) {
+    console.error('Error handling db.json:', error);
+  }
+};
 
 await thanos();
