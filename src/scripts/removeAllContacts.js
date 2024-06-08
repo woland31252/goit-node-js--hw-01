8bec8838cfbeb48d75dfb06762128cba80993202
Volodymyr <110925314+woland31252@users.noreply.github.com>
2024-06-08 18:05:58 +0300

    add removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+        try {
+        await fs.writeFile(PATH_DB, '[]', 'utf-8');
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        let newData = JSON.parse(data);
+        if (newData.length === 0) {
+          console.log('All data has been deleted');
+        } else {
+          console.log('The data was not deleted');
+        }      
+    } catch (error) {
+      console.error('Error: Unable to read file "db.json"', error);
+    }
+};
 
 await removeAllContacts();
